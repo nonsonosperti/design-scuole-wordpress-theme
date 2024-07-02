42bda732df411a51f4921418e62b4af2c7e08072
nonsonosperti <andrea.sperti@innoversa.it>
2024-07-02 18:47:50 +0200

    colors change

--- home.php
+++ home.php
@@ -30,7 +30,7 @@ get_header();
 			<div class="row variable-gutters justify-content-center">
 				<div class="col-md-10">
 					<div class="big-quote">
-						<h2 style="color: #EA7653"><em>il futuro</em> ha il cuore antico.</h2>
+						<h2 style="color: #EA7653; font-size: 5rem"><em>il futuro</em> ha il cuore antico.</h2>
 					</div><!-- /big-quote -->
 				</div><!-- /col-md-10 -->
 			</div><!-- /row -->
